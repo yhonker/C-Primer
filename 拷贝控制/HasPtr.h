5dddc4cad33a97e05cf1c7487ca6cad250ab79ae
yhonker <1497259719@qq.com>
2021-08-16 21:06:55 +0800

    拷贝控制和资源管理

--- 拷贝控制/HasPtr.h
+++ 拷贝控制/HasPtr.h
@@ -8,15 +8,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class HasPtr {
+//错误版本
+class HasPtr1 {
 public:
-    HasPtr(const string &s = string()):ps(new string(s)), i(0){}
-    ~HasPtr(){delete ps;}
+    HasPtr1(const string &s = string()):ps(new string(s)), i(0){}
+    ~HasPtr1(){delete ps;}
     //类定义错误：HasPtr需要一个拷贝构造函数和一个拷贝赋值运算符
 private:
     string *ps;
     int i;
 };
 
+//类值版本
+class HasPtr2{
+public:
+    HasPtr2(const string &s = string()):ps(new string(s)), i(0){}
+    //对ps指向的string，每个HasPtr对象都有自己的拷贝
+    HasPtr2(const HasPtr2 &p):ps(new string(*p.ps)), i(p.i){}
+    HasPtr2 &operator=(const HasPtr2 &rhs);
+    ~HasPtr2(){delete ps;}
+
+private:
+    string *ps;
+    int i;
+};
+
+//指针行为版本--定义一个使用引用计数的类
+class HasPtr3{
+public:
+    //构造函数分配新的string和新的计算器，将计算器置1
+    HasPtr3(const string &s = string()):ps(new string(s)), i(0), use(new size_t(1)){}
+    //拷贝构造函数拷贝所有三个数据成员，并递增计算器
+    HasPtr3(const HasPtr3 &p):ps(p.ps), i(p.i), use(p.use){++*use;}
+    HasPtr3 &operator=(const HasPtr3 &rhs);
+    ~HasPtr3();
+private:
+    string *ps;
+    int i;
+    size_t *use;    //用来记录有多少个对象共享*ps的成员
+};
 
 #endif //CPPTRAIN_HASPTR_H
