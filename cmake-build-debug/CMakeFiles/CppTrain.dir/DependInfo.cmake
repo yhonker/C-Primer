64d5e966d7b7f6e56d5a67359244122336f39fb5
yhonker <1497259719@qq.com>
2021-08-18 21:39:32 +0800

    cpp-primer-class

--- cmake-build-debug/CMakeFiles/CppTrain.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/CppTrain.dir/DependInfo.cmake
@@ -1,24 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/c/Users/yh/Desktop/C++-Primer/main.cpp" "/mnt/c/Users/yh/Desktop/C++-Primer/cmake-build-debug/CMakeFiles/CppTrain.dir/main.cpp.o"
-  "/mnt/c/Users/yh/Desktop/C++-Primer/拷贝控制/HasPtr.cpp" "/mnt/c/Users/yh/Desktop/C++-Primer/cmake-build-debug/CMakeFiles/CppTrain.dir/拷贝控制/HasPtr.cpp.o"
-  "/mnt/c/Users/yh/Desktop/C++-Primer/拷贝控制/Message.cpp" "/mnt/c/Users/yh/Desktop/C++-Primer/cmake-build-debug/CMakeFiles/CppTrain.dir/拷贝控制/Message.cpp.o"
-  "/mnt/c/Users/yh/Desktop/C++-Primer/拷贝控制/NoCopy.cpp" "/mnt/c/Users/yh/Desktop/C++-Primer/cmake-build-debug/CMakeFiles/CppTrain.dir/拷贝控制/NoCopy.cpp.o"
-  "/mnt/c/Users/yh/Desktop/C++-Primer/拷贝控制/Other.cpp" "/mnt/c/Users/yh/Desktop/C++-Primer/cmake-build-debug/CMakeFiles/CppTrain.dir/拷贝控制/Other.cpp.o"
-  "/mnt/c/Users/yh/Desktop/C++-Primer/类/Account.cpp" "/mnt/c/Users/yh/Desktop/C++-Primer/cmake-build-debug/CMakeFiles/CppTrain.dir/类/Account.cpp.o"
-  "/mnt/c/Users/yh/Desktop/C++-Primer/类/Other.cpp" "/mnt/c/Users/yh/Desktop/C++-Primer/cmake-build-debug/CMakeFiles/CppTrain.dir/类/Other.cpp.o"
-  "/mnt/c/Users/yh/Desktop/C++-Primer/类/Sales_data.cpp" "/mnt/c/Users/yh/Desktop/C++-Primer/cmake-build-debug/CMakeFiles/CppTrain.dir/类/Sales_data.cpp.o"
-  "/mnt/c/Users/yh/Desktop/C++-Primer/类/Screen.cpp" "/mnt/c/Users/yh/Desktop/C++-Primer/cmake-build-debug/CMakeFiles/CppTrain.dir/类/Screen.cpp.o"
-  "/mnt/c/Users/yh/Desktop/C++-Primer/类/Window_mgr.cpp" "/mnt/c/Users/yh/Desktop/C++-Primer/cmake-build-debug/CMakeFiles/CppTrain.dir/类/Window_mgr.cpp.o"
+  "/home/honker/CLionProjects/CppTrain/main.cpp" "/home/honker/CLionProjects/CppTrain/cmake-build-debug/CMakeFiles/CppTrain.dir/main.cpp.o"
+  "/home/honker/CLionProjects/CppTrain/拷贝控制/HasPtr.cpp" "/home/honker/CLionProjects/CppTrain/cmake-build-debug/CMakeFiles/CppTrain.dir/拷贝控制/HasPtr.cpp.o"
+  "/home/honker/CLionProjects/CppTrain/拷贝控制/Message.cpp" "/home/honker/CLionProjects/CppTrain/cmake-build-debug/CMakeFiles/CppTrain.dir/拷贝控制/Message.cpp.o"
+  "/home/honker/CLionProjects/CppTrain/拷贝控制/NoCopy.cpp" "/home/honker/CLionProjects/CppTrain/cmake-build-debug/CMakeFiles/CppTrain.dir/拷贝控制/NoCopy.cpp.o"
+  "/home/honker/CLionProjects/CppTrain/拷贝控制/Other.cpp" "/home/honker/CLionProjects/CppTrain/cmake-build-debug/CMakeFiles/CppTrain.dir/拷贝控制/Other.cpp.o"
+  "/home/honker/CLionProjects/CppTrain/类/Account.cpp" "/home/honker/CLionProjects/CppTrain/cmake-build-debug/CMakeFiles/CppTrain.dir/类/Account.cpp.o"
+  "/home/honker/CLionProjects/CppTrain/类/Other.cpp" "/home/honker/CLionProjects/CppTrain/cmake-build-debug/CMakeFiles/CppTrain.dir/类/Other.cpp.o"
+  "/home/honker/CLionProjects/CppTrain/类/Sales_data.cpp" "/home/honker/CLionProjects/CppTrain/cmake-build-debug/CMakeFiles/CppTrain.dir/类/Sales_data.cpp.o"
+  "/home/honker/CLionProjects/CppTrain/类/Screen.cpp" "/home/honker/CLionProjects/CppTrain/cmake-build-debug/CMakeFiles/CppTrain.dir/类/Screen.cpp.o"
+  "/home/honker/CLionProjects/CppTrain/类/Window_mgr.cpp" "/home/honker/CLionProjects/CppTrain/cmake-build-debug/CMakeFiles/CppTrain.dir/类/Window_mgr.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
--- cmake-build-debug/CMakeFiles/CppTrain.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/CppTrain.dir/cmake_clean.cmake
@@ -1,16 +1,16 @@
 file(REMOVE_RECURSE
   "CMakeFiles/CppTrain.dir/main.cpp.o"
+  "CMakeFiles/CppTrain.dir/拷贝控制/HasPtr.cpp.o"
+  "CMakeFiles/CppTrain.dir/拷贝控制/Message.cpp.o"
+  "CMakeFiles/CppTrain.dir/拷贝控制/NoCopy.cpp.o"
+  "CMakeFiles/CppTrain.dir/拷贝控制/Other.cpp.o"
+  "CMakeFiles/CppTrain.dir/类/Account.cpp.o"
+  "CMakeFiles/CppTrain.dir/类/Other.cpp.o"
   "CMakeFiles/CppTrain.dir/类/Sales_data.cpp.o"
   "CMakeFiles/CppTrain.dir/类/Screen.cpp.o"
   "CMakeFiles/CppTrain.dir/类/Window_mgr.cpp.o"
-  "CMakeFiles/CppTrain.dir/类/Other.cpp.o"
-  "CMakeFiles/CppTrain.dir/类/Account.cpp.o"
-  "CMakeFiles/CppTrain.dir/拷贝控制/HasPtr.cpp.o"
-  "CMakeFiles/CppTrain.dir/拷贝控制/Other.cpp.o"
-  "CMakeFiles/CppTrain.dir/拷贝控制/NoCopy.cpp.o"
-  "CMakeFiles/CppTrain.dir/拷贝控制/Message.cpp.o"
-  "CppTrain.pdb"
   "CppTrain"
+  "CppTrain.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
