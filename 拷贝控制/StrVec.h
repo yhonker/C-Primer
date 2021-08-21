2e1fec46ebafa8c1c1b863f6eb202e81fa96c1f1
yhonker <1497259719@qq.com>
2021-08-21 21:49:48 +0800

    拷贝控制

--- 拷贝控制/StrVec.h
+++ 拷贝控制/StrVec.h
@@ -4,11 +4,30 @@
 
 #ifndef CPPTRAIN_STRVEC_H
 #define CPPTRAIN_STRVEC_H
-
-
+#include <bits/stdc++.h>
+using namespace std;
 class StrVec {
-
+public:
+    StrVec():elements(nullptr),first_free(nullptr),cap(nullptr){ }
+    StrVec(const StrVec&);
+    StrVec &operator=(const StrVec&);
+    ~StrVec();
+    void push_back(const string&);
+    size_t size() const { return first_free - elements;}
+    size_t capacity() const{ return cap - elements;}
+    string *begin() const {return elements;}
+    string *end() const {return first_free;}
+private:
+    static allocator<string> alloc;//
+    void chk_n_alloc(){if(size() == capacity()) reallocate();}
+    pair<string*, string*> alloc_n_copy(const string*, const string*);
+    void free();
+    void reallocate();
+    string *elements;
+    string *first_free;
+    string *cap;
 };
+
 
 
 #endif //CPPTRAIN_STRVEC_H
