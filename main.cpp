eede8a8dbb467856e5999006550dd2adc4a40c0e
“yhonker” <1497259719@qq.com>
2022-01-16 16:42:34 +0800

    运算符重载

--- main.cpp
+++ main.cpp
@@ -3,23 +3,8 @@
 #include "类/Screen.h"
 #include "类/Window_mgr.h"
 
-class NoDefault{
-public:
-    NoDefault() = default;
-    NoDefault(const string&);
-};
 
-struct st{
+int main()
+{
 
-    NoDefault my_mem;
-};
-int main() {
-    Screen sc(50, 50, 's');
-    sc.move(2, 2).display(cout).set('h').display(cout);
-    cout<<sc.get(2, 2)<<endl;
-
-
-
-    st s1;
-    return 0;
 }
